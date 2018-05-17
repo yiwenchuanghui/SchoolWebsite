eaf1621dec048faefd980f94d798c535ac8fd30e
DastanSE <dastan.zhanibek@gmail.com>
2018-05-17 17:22:46 +0800

    version 0.0.3

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+// blog home page
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/application', (req, res) => {
+  res.render('pages/application');
+});
+app.get('/admissions', (req, res) => {
+  res.render('pages/admissions');
+});
+
+app.listen(5555)
+
+console.log('listening on port 5555')
